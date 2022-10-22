a605a236c9d99cd0831e259235b53b485fae9d41
Revan0402 <ralizade198@gmail.com>
2022-10-22 10:44:41 +0400

    update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var seyahet = [
+    ["Tebiet",
+        ["Deniz","Hava limani"],"Ismayilli","Almaniya"
+    ],
+    ["Aytac","Gultac","Sema","Nubar","Azima-Esmira"],
+    ["Revan","Sebuhi","Rza","Deyanet"],
+    [
+        "Shah dag",["Yay","Qis"],"Mandarin",
+    ],
+    ["","Baki."]
+];
