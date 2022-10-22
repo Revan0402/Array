19ff7f9a23bc015bce4eadaa98aa52ee59bc47c4
Revan0402 <ralizade198@gmail.com>
2022-10-22 11:07:31 +0400

    finish

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ var seyahet = [
     ],
     ["","Baki."]
 ];
+
+console.log(seyahet[3][2][0]+seyahet[0][1][0][1]+seyahet[2][3][4]+
+    seyahet[3][1][1][1]+seyahet[0][3][2]+seyahet[3][0][4]+
+    seyahet[1][3][3]+seyahet[3][2][3]+seyahet[2][1][5]+seyahet[1][2][2]+
+    seyahet[0][1][1][4]+seyahet[2][0][0]+seyahet[0][0][1]+seyahet[0][1][1][2]+
+    seyahet[4][1][1]+seyahet[0][3][4]+seyahet[1][4][5]+seyahet[3][0][5]+seyahet[2][1][5]+
+    seyahet[1][4][10]+seyahet[4][1][4]
+)
